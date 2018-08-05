7995e11d344802a3e68ca139dff09a6b901318ee
Duc Hoang Nguyen <lazycoder97@gmail.com>
2018-08-06 00:57:30 +0700

    class controller

--- tornado/controllers/api/ClassController.py
+++ tornado/controllers/api/ClassController.py
@@ -1,21 +1,70 @@
+import json
+
+from bson.objectid import ObjectId
+
 from .BaseApiHandler import BaseApiHandler
+
 
 class ClassController(BaseApiHandler):
     # get classes by semester
     async def get(self):
-        pass
+        self.checkUserAccess()
+        result = self.settings["db"]["classes"].find({
+            "semesterId": ObjectId(self.json["semesterId"])
+        })
+
+        items = []
+        async for item in result:
+            item["_id"] = str(item["_id"])
+            item["semesterId"] = str(item["semesterId"])
+            items.append(item)
+
+        self.write(json.dumps({
+            "semesterId": self.json["semesterId"],
+            "classes": items,
+        }))
+
 
     # add a class
     async def post(self):
         self.checkAdminAccess()
-        pass
+        year = {
+            "name": str(self.json["name"]),
+            "code": str(self.json["code"]),
+            "scorePdfLink": None,
+            "semesterId": ObjectId(self.json["semesterId"]),
+        }
+        result = await self.settings["db"]["classes"].insert_one(year)
+        self.write(json.dumps({
+            "_id": str(result.inserted_id),
+        }))
+
 
     # update a class
     async def put(self):
         self.checkAdminAccess()
-        pass
+        result = await self.settings["db"]["classes"].update_one({
+            "_id": ObjectId(self.json["_id"]),
+        }, {
+            "$set": {
+                "name": str(self.json["name"]),
+                "code": str(self.json["code"]),
+                "semesterId": ObjectId(self.json["semesterId"]),
+            }
+        })
+
+        self.write(json.dumps({
+            "modified": result.modified_count,
+        }))
+
 
     # delete a class
     async def delete(self):
         self.checkAdminAccess()
-        pass
+        result = await self.settings["db"]["classes"].delete_one({
+            "_id": ObjectId(self.json["_id"]),
+        })
+
+        self.write(json.dumps({
+            "deleted": result.deleted_count,
+        }))
